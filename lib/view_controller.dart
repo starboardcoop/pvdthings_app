4134bda0ea8eac4549fe20a5d74ca96961037f03
Dillon Fagan <dillonfagan@pm.me>
2021-11-24 14:29:05 -0500

    Remove unused code

--- lib/view_controller.dart
+++ lib/view_controller.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:pvd_things_app/views/browse.dart';
-import 'package:pvd_things_app/views/me.dart';
-import 'package:pvd_things_app/views/polls.dart';
 
 class ViewController extends StatefulWidget {
   const ViewController({Key? key}) : super(key: key);
@@ -13,23 +11,10 @@ class ViewController extends StatefulWidget {
 }
 
 class _ViewControllerState extends State<ViewController> {
-  int _selectedPage = 0;
-  static const List<Widget> _pages = <Widget>[
-    BrowsePage(),
-    PollsPage(),
-    MePage(),
-  ];
-
-  void _updatePage(int index) {
-    setState(() {
-      _selectedPage = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages.elementAt(_selectedPage),
+    return const Scaffold(
+      body: BrowsePage(),
     );
   }
 }
